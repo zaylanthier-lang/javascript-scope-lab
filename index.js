8fe1905b2e75133569ea85118a8eb4948a54abb3
Zayden <zaylanthier@gmail.com>
2026-03-13 12:48:50 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// Write your solution in this file!
+
+//  Variable Data
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+//  Function and Block
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+//  Variable Declaration and Scope
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
